7b431db82520e26a5f4e420d287874a7e6b722ca
xjlmx521 <xjlmx521@gmail.com>
2017-09-11 10:53:02 +0800

    Update tlist.h

--- tlist.h
+++ tlist.h
@@ -1,102 +1,46 @@
-/****************************************************************************
- *
- * MODULE:             ZigbeeNodeControlBridge
- *
- * COMPONENT:          Serial Link to Host
- *
- * VERSION:
- *
- * REVISION:           $$
- *
- * DATED:              $$
- *
- * STATUS:             $State: Exp $
- *
- * AUTHOR:             Lee Mitchell
- *
- * DESCRIPTION:
- *
- * LAST MODIFIED BY:   $Author: nxp29741 $
- *                     $Modtime: $
- *
- *
- ****************************************************************************
- *
- * This software is owned by NXP B.V. and/or its supplier and is protected
- * under applicable copyright laws. All rights are reserved. We grant You,
- * and any third parties, a license to use this software solely and
- * exclusively on NXP products [NXP Microcontrollers such as JN5168, JN5179].
- * You, and any third parties must reproduce the copyright and warranty notice
- * and any other legend of ownership on each copy or partial copy of the
- * software.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * Copyright NXP B.V. 2016. All rights reserved
- *
- ****************************************************************************/
 
-#ifndef  SERIALLINK_H_INCLUDED
-#define  SERIALLINK_H_INCLUDED
+#ifndef _TN_LIST_H_
+#define _TN_LIST_H_
 
-/****************************************************************************/
-/***        Include files                                                 ***/
-/****************************************************************************/
-#include "../../../proj/tl_common.h"
+#define STRING_CONCAT(s1, s2) s1##s2
 
-/****************************************************************************/
-/***        Macro Definitions                                             ***/
-/****************************************************************************/
+typedef void ** tn_list_t;
 
-#define SL_WRITE(DATA)        bPutChar(DATA)
+//Define a list
+#define LIST_DEF(name) \
+        static void *STRING_CONCAT(name,_list) = 0; \
+        static tn_list_t name = (tn_list_t)&STRING_CONCAT(name,_list)
 
-#define SL_START_CHAR          0x01
-#define SL_ESC_CHAR            0x02
-#define SL_END_CHAR            0x03
-typedef enum{
-	SL_CONVERT_RET_WAITINGMOREDATA		= 0xf0000000,
-	SL_CONVERT_RET_CONTINUEPACKET		= 0xf0000001
-}sl_convertRet_e;
-/****************************************************************************/
-/***        Local Function Prototypes                                     ***/
-/****************************************************************************/
+//Define a list inside a structure
+#define LIST_STRUCT_DEF(name) \
+        void *STRING_CONCAT(name,_list); \
+        tn_list_t name
 
-/****************************************************************************/
-/***        Exported Variables                                            ***/
-/****************************************************************************/
+//Initialize a list that is a member of a structure
+#define LIST_STRUCT_INIT(struct_ptr, name)                              \
+    do {                                                                \
+        (struct_ptr)->name = &((struct_ptr)->STRING_CONCAT(name,_list));   \
+        (struct_ptr)->STRING_CONCAT(name,_list) = 0;                    \
+        tn_list_init((struct_ptr)->name);                                   \
+    } while(0)
 
-/****************************************************************************/
-/***        Local Variables                                               ***/
-/****************************************************************************/
+void   tn_list_init(tn_list_t list);
+void * tn_list_head(tn_list_t list);
+void * tn_list_tail(tn_list_t list);
+void * tn_list_pop (tn_list_t list);
+void   tn_list_push(tn_list_t list, void *item);
 
-/****************************************************************************/
-/***        Exported Functions                                            ***/
-/****************************************************************************/
+void * tn_list_chop(tn_list_t list);
 
-bool bSL_ReadMessage(u16 *pu16Type, u16 *pu16Length, u16 u16MaxLength, u8 *pu8Message,u8 u8Byte);
-void vSL_WriteMessage(u16 u16Type, u16 u16Length, u8 *pu8Data);
-u8 u8SL_CalculateCRC(u16 u16Type, u16 u16Length, u8 *pu8Data);
-/****************************************************************************/
-/***        Local Functions                                               ***/
-/****************************************************************************/
+void   tn_list_add(tn_list_t list, void *item);
+void   tn_list_remove(tn_list_t list, void *item);
 
-#if defined __cplusplus
-}
-#endif
+int    tn_list_length(tn_list_t list);
 
-#endif  /* SERIALLINK_H_INCLUDED */
+void   tn_list_copy(tn_list_t dest, tn_list_t src);
 
-/****************************************************************************/
-/***        END OF FILE                                                   ***/
-/****************************************************************************/
+void   tn_list_insert(tn_list_t list, void *previtem, void *newitem);
 
+void * tn_list_item_next(void *item);
+
+#endif /* _TN_LIST_H_ */
